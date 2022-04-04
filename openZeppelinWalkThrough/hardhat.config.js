b798c2b2d8d8c92d17980a25bf47a0885f47c795
Michael Burke <michael.dburke56@gmail.com>
2022-04-04 09:59:45 -0400

    Add rinkeby testnet config.

--- openZeppelinWalkThrough/hardhat.config.js
+++ openZeppelinWalkThrough/hardhat.config.js
@@ -1,8 +1,18 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- require('@nomiclabs/hardhat-ethers');
- require("@nomiclabs/hardhat-truffle5");
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-truffle5");
+
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
 module.exports = {
-  solidity: "0.8.4",
+    solidity: "0.8.4",
+    networks: {
+        rinkeby: {
+            url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+            accounts: { mnemonic: mnemonic },
+        },
+    },
 };
+
+
