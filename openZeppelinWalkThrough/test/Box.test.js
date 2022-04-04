2b54bc44c1988ac3a3125236eb9866fae1c50319
Michael Burke <michael.dburke56@gmail.com>
2022-04-03 21:03:55 -0400

    Import web3 and truffle for hardhat and install OZ test lib for more comprehensive testing.

--- openZeppelinWalkThrough/test/Box.test.js
+++ openZeppelinWalkThrough/test/Box.test.js
@@ -1,17 +1,41 @@
+// test/Box.test.js
+// Load dependencies
 const { expect } = require('chai');
-const { ethers } = require('hardhat');
-const { isCallTrace } = require('hardhat/internal/hardhat-network/stack-traces/message-trace');
-describe('Box', function () {
-  before(async function () {
-    this.Box = await ethers.getContractFactory('Box');
-  });
+
+// Import utilities from Test Helpers
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+
+// Load compiled artifacts
+const Box = artifacts.require('Box');
+
+// Start test block
+contract('Box', function ([ owner, other ]) {
+  // Use large integers ('big numbers')
+  const value = new BN('42');
+
   beforeEach(async function () {
-    this.box = await this.Box.deploy();
-    await this.box.deployed();
+    this.box = await Box.new({ from: owner });
   });
 
-  it('retrieve returns a value poreviously stores', async function () {
-    await this.box.store(42);
-    expect((await this.box.retrieve()).toString()).to.equal('42');
+  it('retrieve returns a value previously stored', async function () {
+    await this.box.store(value, { from: owner });
+
+    // Use large integer comparisons
+    expect(await this.box.retrieve()).to.be.bignumber.equal(value);
+  });
+
+  it('store emits an event', async function () {
+    const receipt = await this.box.store(value, { from: owner });
+
+    // Test that a ValueChanged event was emitted with the new value
+    expectEvent(receipt, 'ValueChanged', { value: value });
+  });
+
+  it('non owner cannot store a value', async function () {
+    // Test a transaction reverts
+    await expectRevert(
+      this.box.store(value, { from: other }),
+      'Ownable: caller is not the owner',
+    );
   });
 });
